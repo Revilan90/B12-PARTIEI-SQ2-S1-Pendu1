95de47e54beb80a764949d25979c1845a8bb5c0f
Revilan90 <johannes.massiani@yahoo.fr>
2022-10-21 15:40:55 +0200

    vite

--- B12-PARTIEI-SQ2-S1-Pendu1/Form1.Designer.cs
+++ B12-PARTIEI-SQ2-S1-Pendu1/Form1.Designer.cs
@@ -29,106 +29,99 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbPendu = new System.Windows.Forms.PictureBox();
             this.cbAlphabet = new System.Windows.Forms.ComboBox();
             this.lblMotATrouver = new System.Windows.Forms.Label();
             this.txtbSaisieMot = new System.Windows.Forms.TextBox();
             this.btnValider = new System.Windows.Forms.Button();
-            this.btnEffacer = new System.Windows.Forms.Button();
             this.btnRelancer = new System.Windows.Forms.Button();
-            this.btnQuitter = new System.Windows.Forms.Button();
             this.lblGagnePerdu = new System.Windows.Forms.Label();
             this.lstbDejaEssaye = new System.Windows.Forms.ListBox();
             this.btnTest = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.grpPhase2 = new System.Windows.Forms.GroupBox();
+            this.grpPhase1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPendu)).BeginInit();
+            this.grpPhase2.SuspendLayout();
+            this.grpPhase1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pbPendu
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(9, 10);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(135, 138);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pbPendu.Location = new System.Drawing.Point(18, 13);
+            this.pbPendu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pbPendu.Name = "pbPendu";
+            this.pbPendu.Size = new System.Drawing.Size(180, 170);
+            this.pbPendu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPendu.TabIndex = 0;
+            this.pbPendu.TabStop = false;
             // 
             // cbAlphabet
             // 
+            this.cbAlphabet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbAlphabet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbAlphabet.FormattingEnabled = true;
-            this.cbAlphabet.Location = new System.Drawing.Point(293, 95);
-            this.cbAlphabet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbAlphabet.Location = new System.Drawing.Point(176, 51);
+            this.cbAlphabet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbAlphabet.Name = "cbAlphabet";
-            this.cbAlphabet.Size = new System.Drawing.Size(84, 24);
+            this.cbAlphabet.Size = new System.Drawing.Size(111, 28);
             this.cbAlphabet.TabIndex = 1;
+            this.cbAlphabet.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cbAlphabet_KeyPress);
             // 
             // lblMotATrouver
             // 
             this.lblMotATrouver.AutoSize = true;
-            this.lblMotATrouver.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMotATrouver.Location = new System.Drawing.Point(148, 65);
-            this.lblMotATrouver.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMotATrouver.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMotATrouver.Location = new System.Drawing.Point(6, 17);
             this.lblMotATrouver.Name = "lblMotATrouver";
-            this.lblMotATrouver.Size = new System.Drawing.Size(184, 17);
+            this.lblMotATrouver.Size = new System.Drawing.Size(321, 29);
             this.lblMotATrouver.TabIndex = 2;
             this.lblMotATrouver.Text = "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _";
             // 
             // txtbSaisieMot
             // 
             this.txtbSaisieMot.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtbSaisieMot.Location = new System.Drawing.Point(157, 22);
+            this.txtbSaisieMot.Location = new System.Drawing.Point(12, 17);
+            this.txtbSaisieMot.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbSaisieMot.MaxLength = 15;
             this.txtbSaisieMot.Name = "txtbSaisieMot";
-            this.txtbSaisieMot.Size = new System.Drawing.Size(128, 23);
+            this.txtbSaisieMot.Size = new System.Drawing.Size(169, 26);
             this.txtbSaisieMot.TabIndex = 3;
+            this.txtbSaisieMot.TextChanged += new System.EventHandler(this.txtbSaisieMot_TextChanged);
+            this.txtbSaisieMot.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbSaisieMot_KeyPress);
             // 
             // btnValider
             // 
             this.btnValider.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnValider.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnValider.BackgroundImage")));
             this.btnValider.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnValider.Location = new System.Drawing.Point(293, 10);
+            this.btnValider.Location = new System.Drawing.Point(242, 3);
+            this.btnValider.Margin = new System.Windows.Forms.Padding(4);
             this.btnValider.Name = "btnValider";
-            this.btnValider.Size = new System.Drawing.Size(47, 52);
+            this.btnValider.Size = new System.Drawing.Size(63, 64);
             this.btnValider.TabIndex = 4;
             this.btnValider.UseVisualStyleBackColor = true;
-            // 
-            // btnEffacer
-            // 
-            this.btnEffacer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEffacer.BackgroundImage")));
-            this.btnEffacer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnEffacer.Location = new System.Drawing.Point(346, 10);
-            this.btnEffacer.Name = "btnEffacer";
-            this.btnEffacer.Size = new System.Drawing.Size(47, 52);
-            this.btnEffacer.TabIndex = 5;
-            this.btnEffacer.UseVisualStyleBackColor = true;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
             // 
             // btnRelancer
             // 
             this.btnRelancer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRelancer.BackgroundImage")));
             this.btnRelancer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnRelancer.Location = new System.Drawing.Point(293, 124);
+            this.btnRelancer.Location = new System.Drawing.Point(177, 85);
+            this.btnRelancer.Margin = new System.Windows.Forms.Padding(4);
             this.btnRelancer.Name = "btnRelancer";
-            this.btnRelancer.Size = new System.Drawing.Size(47, 50);
+            this.btnRelancer.Size = new System.Drawing.Size(63, 62);
             this.btnRelancer.TabIndex = 6;
             this.btnRelancer.UseVisualStyleBackColor = true;
-            // 
-            // btnQuitter
-            // 
-            this.btnQuitter.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnQuitter.BackgroundImage")));
-            this.btnQuitter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnQuitter.Location = new System.Drawing.Point(346, 124);
-            this.btnQuitter.Name = "btnQuitter";
-            this.btnQuitter.Size = new System.Drawing.Size(99, 50);
-            this.btnQuitter.TabIndex = 7;
-            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnRelancer.Click += new System.EventHandler(this.btnRelancer_Click);
             // 
             // lblGagnePerdu
             // 
             this.lblGagnePerdu.AutoSize = true;
             this.lblGagnePerdu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblGagnePerdu.Location = new System.Drawing.Point(12, 161);
+            this.lblGagnePerdu.Location = new System.Drawing.Point(16, 198);
+            this.lblGagnePerdu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblGagnePerdu.Name = "lblGagnePerdu";
-            this.lblGagnePerdu.Size = new System.Drawing.Size(134, 24);
+            this.lblGagnePerdu.Size = new System.Drawing.Size(168, 29);
             this.lblGagnePerdu.TabIndex = 8;
             this.lblGagnePerdu.Text = "Perdu/Gagne";
             // 
@@ -136,43 +129,68 @@
             // 
             this.lstbDejaEssaye.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstbDejaEssaye.FormattingEnabled = true;
-            this.lstbDejaEssaye.ItemHeight = 16;
-            this.lstbDejaEssaye.Location = new System.Drawing.Point(157, 95);
+            this.lstbDejaEssaye.ItemHeight = 20;
+            this.lstbDejaEssaye.Location = new System.Drawing.Point(10, 48);
+            this.lstbDejaEssaye.Margin = new System.Windows.Forms.Padding(4);
             this.lstbDejaEssaye.Name = "lstbDejaEssaye";
-            this.lstbDejaEssaye.Size = new System.Drawing.Size(120, 84);
+            this.lstbDejaEssaye.Size = new System.Drawing.Size(159, 84);
             this.lstbDejaEssaye.TabIndex = 9;
             // 
             // btnTest
             // 
             this.btnTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTest.Location = new System.Drawing.Point(382, 95);
+            this.btnTest.Location = new System.Drawing.Point(294, 50);
+            this.btnTest.Margin = new System.Windows.Forms.Padding(4);
             this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(63, 23);
+            this.btnTest.Size = new System.Drawing.Size(84, 28);
             this.btnTest.TabIndex = 10;
             this.btnTest.Text = "TEST";
             this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // grpPhase2
+            // 
+            this.grpPhase2.Controls.Add(this.btnTest);
+            this.grpPhase2.Controls.Add(this.lstbDejaEssaye);
+            this.grpPhase2.Controls.Add(this.lblMotATrouver);
+            this.grpPhase2.Controls.Add(this.btnRelancer);
+            this.grpPhase2.Controls.Add(this.cbAlphabet);
+            this.grpPhase2.Location = new System.Drawing.Point(198, 95);
+            this.grpPhase2.Name = "grpPhase2";
+            this.grpPhase2.Size = new System.Drawing.Size(387, 162);
+            this.grpPhase2.TabIndex = 11;
+            this.grpPhase2.TabStop = false;
+            this.grpPhase2.Text = "Recherche";
+            // 
+            // grpPhase1
+            // 
+            this.grpPhase1.Controls.Add(this.btnValider);
+            this.grpPhase1.Controls.Add(this.txtbSaisieMot);
+            this.grpPhase1.Location = new System.Drawing.Point(197, 10);
+            this.grpPhase1.Name = "grpPhase1";
+            this.grpPhase1.Size = new System.Drawing.Size(318, 79);
+            this.grpPhase1.TabIndex = 12;
+            this.grpPhase1.TabStop = false;
+            this.grpPhase1.Text = "Veuillez saisir un mot à chercher";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(472, 200);
-            this.Controls.Add(this.btnTest);
-            this.Controls.Add(this.lstbDejaEssaye);
+            this.ClientSize = new System.Drawing.Size(593, 269);
+            this.Controls.Add(this.grpPhase1);
+            this.Controls.Add(this.grpPhase2);
             this.Controls.Add(this.lblGagnePerdu);
-            this.Controls.Add(this.btnQuitter);
-            this.Controls.Add(this.btnRelancer);
-            this.Controls.Add(this.btnEffacer);
-            this.Controls.Add(this.btnValider);
-            this.Controls.Add(this.txtbSaisieMot);
-            this.Controls.Add(this.lblMotATrouver);
-            this.Controls.Add(this.cbAlphabet);
-            this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.pbPendu);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Jeux du Pendu";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPendu)).EndInit();
+            this.grpPhase2.ResumeLayout(false);
+            this.grpPhase2.PerformLayout();
+            this.grpPhase1.ResumeLayout(false);
+            this.grpPhase1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,17 +198,17 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbPendu;
         private System.Windows.Forms.ComboBox cbAlphabet;
         private System.Windows.Forms.Label lblMotATrouver;
         private System.Windows.Forms.TextBox txtbSaisieMot;
         private System.Windows.Forms.Button btnValider;
-        private System.Windows.Forms.Button btnEffacer;
         private System.Windows.Forms.Button btnRelancer;
-        private System.Windows.Forms.Button btnQuitter;
         private System.Windows.Forms.Label lblGagnePerdu;
         private System.Windows.Forms.ListBox lstbDejaEssaye;
         private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.GroupBox grpPhase2;
+        private System.Windows.Forms.GroupBox grpPhase1;
     }
 }
 
--- B12-PARTIEI-SQ2-S1-Pendu1/Properties/Resources.Designer.cs
+++ B12-PARTIEI-SQ2-S1-Pendu1/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace B12_PARTIEI_SQ2_S1_Pendu1.Properties
-{
-
-
+namespace B12_PARTIEI_SQ2_S1_Pendu1.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   Une classe de ressource fortement typée destinée, entre autres, à la consultation des chaînes localisées.
     /// </summary>
@@ -19,52 +19,154 @@ namespace B12_PARTIEI_SQ2_S1_Pendu1.Properties
     // à l'aide d'un outil, tel que ResGen ou Visual Studio.
     // Pour ajouter ou supprimer un membre, modifiez votre fichier .ResX, puis réexécutez ResGen
     // avec l'option /str ou régénérez votre projet VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Retourne l'instance ResourceManager mise en cache utilisée par cette classe.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("B12_PARTIEI_SQ2_S1_Pendu1.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Remplace la propriété CurrentUICulture du thread actuel pour toutes
         ///   les recherches de ressources à l'aide de cette classe de ressource fortement typée.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pendu0 {
+            get {
+                object obj = ResourceManager.GetObject("pendu0", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pendu1 {
+            get {
+                object obj = ResourceManager.GetObject("pendu1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pendu10 {
+            get {
+                object obj = ResourceManager.GetObject("pendu10", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pendu2 {
+            get {
+                object obj = ResourceManager.GetObject("pendu2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pendu3 {
+            get {
+                object obj = ResourceManager.GetObject("pendu3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pendu4 {
+            get {
+                object obj = ResourceManager.GetObject("pendu4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pendu5 {
+            get {
+                object obj = ResourceManager.GetObject("pendu5", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pendu6 {
+            get {
+                object obj = ResourceManager.GetObject("pendu6", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pendu7 {
+            get {
+                object obj = ResourceManager.GetObject("pendu7", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pendu8 {
+            get {
+                object obj = ResourceManager.GetObject("pendu8", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pendu9 {
+            get {
+                object obj = ResourceManager.GetObject("pendu9", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
